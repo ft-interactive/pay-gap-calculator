d0871c9bf2c1a1669aef6d7a6e97d8ae6f4d66ce
Joanna Kao <joannaskao@gmail.com>
2017-11-09 23:22:34 -0500

    use more updated visual journalism list

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default (environment = 'development') => ({ // eslint-disable-line
   relatedContent: [
     { rows: 1, list: 'thing/5aef15c3-9456-3eaf-8b5d-9e08413beffb' },
-    { rows: 1, list: 'list/graphics' },
+    { rows: 1, list: 'thing/adae44ca-4ea7-3cf8-8332-bf85ec89a558' },
   ],
 });
